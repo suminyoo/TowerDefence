e21f6dabd8ee2a1854ab703822ee9943a4353e49
suminyoo <bluebell8400@gmail.com>
2025-04-11 21:51:01 +0900

    day2

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -9,6 +9,27 @@ public class Enemy : MonoBehaviour
 
     public int HP = 100;
     public int ATK;
+
+    public ParticleSystem MuzzelFlash_ParticlaSystem;
+    public ParticleSystem BulletShells_ParticleSystem;
+    public ParticleSystem Traser_ParticleSystem;
+
+
+    //ParticleSystem 정의 
+    //헤더로 Enemy(Script)에 알아보기 쉽게
+    [Header("Ps for Destroy")]
+    //정의한 후 추가할 파티클 드래그해서 추가
+    public ParticleSystem Destroy_ParticleSystem;
+
+    //Awake로 시작
+    private void Awake()
+    {
+        Destroy_ParticleSystem.Stop();          //처음에 Awake하고 Stop으로 제어
+        //MuzzelFlash_ParticlaSystem.Stop();
+        //BulletShells_ParticleSystem.Stop();
+        //Traser_ParticleSystem.Stop();
+    }
+
     public void CheckHP(int damage)
     {
         HP = HP - damage;
@@ -31,11 +52,14 @@ public class Enemy : MonoBehaviour
         CheckHP(10);
         Debug.Log("OnTriggerEnter!!!!!" + other.name);
     }
+
     private void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.A) )
+        {
+            Destroy_ParticleSystem.Play();
+        }
         gunbarrel.LookAt(RealTarget);
+
     }
-
-
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,13 +6,28 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Enemy enemy;
-
     public Turret turret;
-   
+
+    public GameObject enemyPrefab;
+    public GameObject turretPrefab;
+    public Transform world;
+    public Transform battleField;
+
 
 
     void Start()
     {
+        int xPos = UnityEngine.Random.Range(2, 10);
+        int zPos = UnityEngine.Random.Range(2, 10);
+
+        //vector position
+        Vector3 pos = new Vector3(xPos, 0, zPos);
+
+        //생성
+        //Instantiate(enemyPrefab, pos, Quaternion.identity).transform.SetParent(world);
+        //생성한 world의 자식으로 들어감 (Script메뉴에서 드래그 설정)
+        Initialize();
+
         enemy.HP = UnityEngine.Random.Range(100, 150);
         turret.HP = UnityEngine.Random.Range(100, 150);
 
@@ -21,5 +36,17 @@ public class GameManager : MonoBehaviour
 
     }
 
-   
+    void Initialize()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            int xPos = UnityEngine.Random.Range(-5, 10);
+            int zPos = UnityEngine.Random.Range(-5, 10);
+
+            Vector3 pos = new Vector3(xPos, 0, zPos);
+            Instantiate(enemyPrefab, pos, Quaternion.identity).transform.SetParent(battleField);
+        }
+    }
+
+
 }
